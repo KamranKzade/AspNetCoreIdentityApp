e5dd3ef9a3c2a7c997c50e32d131b1f2ecab91a8
Kamran <311.kamran@gmail.com>
2023-08-17 17:09:46 +0400

    SignUp( Uye ol ) Action Method kodlamasi

--- AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
@@ -1,39 +1,73 @@
 ﻿using AspNetCoreIdentityApp.Web.Models;
+using AspNetCoreIdentityApp.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace AspNetCoreIdentityApp.Web.Controllers
+namespace AspNetCoreIdentityApp.Web.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-       
-        public IActionResult SignUp()
-        {
-            return View();
-        }
+	// Kullanici ile bagli butun isleri heyata keciren classdir
+	private readonly UserManager<AppUser> _userManager;
 
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+	public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
+	{
+		_logger = logger;
+		_userManager = userManager;
+	}
+
+	public IActionResult Index()
+	{
+		return View();
+	}
+
+	public IActionResult Privacy()
+	{
+		return View();
+	}
+
+
+	public IActionResult SignUp()
+	{
+		return View();
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> SignUp(SignUpViewModel request)
+	{
+		// identity-ni userManager ile yaratmaga calisiriq
+		var identityResult = await _userManager.CreateAsync(new AppUser
+		{
+			UserName = request.Username,
+			PhoneNumber = request.Phone,
+			Email = request.Email
+		},
+		request.PasswordConfirm);
+
+		// identity ugurla bas veribse ViewBag ile ekrana melumat otururuk.
+		if (identityResult.Succeeded)
+		{
+			TempData["SuccessMessage"] = "Uyelik kayit islemi basarili ile gerceklemistir";
+			return RedirectToAction(nameof(HomeController.SignUp));
+		}
+
+		// identity ugurla olmasa, bu zaman artiq erroru ekrana cixaririq.
+		foreach (IdentityError item in identityResult.Errors)
+		{
+			ModelState.AddModelError(string.Empty, item.Description);
+		}
+
+		return View();
+	}
+
+
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
 }
