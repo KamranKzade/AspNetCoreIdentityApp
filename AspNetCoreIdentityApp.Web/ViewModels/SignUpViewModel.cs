63b529e442dea472491aa8ba3c895e924abe36d3
Kamran <311.kamran@gmail.com>
2023-08-17 16:24:06 +0400

    SignUp( Uye ol ) sayfasinin tasarlanmasi

---
+++ AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels;
+
+
+public class SignUpViewModel
+{
+    [Display(Name ="Kullanici Adi: ")]
+    public string Username { get; set; }
+    [Display(Name = "Email: ")]
+    public string Email { get; set; }
+    [Display(Name = "Telefon: ")]
+    public string Phone { get; set; }
+    [Display(Name = "Parol: ")]
+    public string Password { get; set; }
+
+    [Display(Name = "Parol Tekrar: ")]
+    public string PasswordConfirm { get; set; }
+
+
+    public SignUpViewModel() { }
+
+
+    public SignUpViewModel(string? username, string? email, string? phone, string? password)
+    {
+        Username = username;
+        Email = email;
+        Phone = phone;
+        Password = password;
+    }
+
+}
