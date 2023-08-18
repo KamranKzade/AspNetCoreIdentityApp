c2e17104911faa4448a7ada7c8406510fe840227
Kamran <311.kamran@gmail.com>
2023-08-18 16:19:25 +0400

    Password Validation Options

---
+++ AspNetCoreIdentityApp.Web/Extentions/StartUpExtensions.cs
@@ -0,0 +1,26 @@
+﻿using AspNetCoreIdentityApp.Web.Models;
+
+namespace AspNetCoreIdentityApp.Web.Extentions;
+
+
+// Program.cs file-nin pis gune qalmamasi ucun extention methodda yaziriq burani
+public static class StartUpExtensions
+{
+	public static void AddIdentityWithExtention(this IServiceCollection services)
+	{
+		services.AddIdentity<AppUser, AppRole>(opt =>
+		{
+			// Identity-nin icerisinde gelen default validationlari oz isteyimize uygun duzeldirik
+			opt.User.RequireUniqueEmail = true;
+			opt.User.AllowedUserNameCharacters = "qwertyuiopasdfghjklzxcvbnm1234567890_";
+
+			opt.Password.RequiredLength = 6;
+			opt.Password.RequireNonAlphanumeric = false;
+			opt.Password.RequireLowercase = true;
+			opt.Password.RequireUppercase = true;
+			opt.Password.RequireDigit = false;
+
+
+		}).AddEntityFrameworkStores<AppDbContext>();
+	}
+}
--- AspNetCoreIdentityApp.Web/Program.cs
+++ AspNetCoreIdentityApp.Web/Program.cs
@@ -1,3 +1,4 @@
+using AspNetCoreIdentityApp.Web.Extentions;
 using AspNetCoreIdentityApp.Web.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,23 +7,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
 });
 
-
-builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<AppDbContext>();
+// Identittyni sisteme elave edirik
+builder.Services.AddIdentityWithExtention();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -38,7 +38,7 @@ app.MapControllerRoute(
 	pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
