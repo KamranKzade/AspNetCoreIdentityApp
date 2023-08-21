b5ab13961c8bd92d531a42abdf8b1ed334bed220
Kamran <311.kamran@gmail.com>
2023-08-21 14:19:15 +0400

    Uyelerin erisebilecegi sayfalar olusturmak
    Authorize atribute

--- AspNetCoreIdentityApp.Web/Controllers/MemberController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/MemberController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using AspNetCoreIdentityApp.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AspNetCoreIdentityApp.Web.Controllers;
 
+[Authorize]
 public class MemberController : Controller
 {
 	private readonly SignInManager<AppUser> _signInManager;
@@ -13,6 +15,13 @@ public class MemberController : Controller
 	{
 		_signInManager = signInManager;
 	}
+
+
+	public IActionResult Index()
+	{
+		return View();
+	}
+
 
 	public async Task LogOut()
 	{
