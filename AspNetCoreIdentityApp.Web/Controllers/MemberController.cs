f5f612df001b7dc0c049ca5216b8741015831a71
Kamran <311.kamran@gmail.com>
2023-08-21 13:45:59 +0400

    Cikis yap action methodunun kodlanmasi
    sign sehfesinnen cixmaq ucun lazim olan method

---
+++ AspNetCoreIdentityApp.Web/Controllers/MemberController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using AspNetCoreIdentityApp.Web.Models;
+
+namespace AspNetCoreIdentityApp.Web.Controllers;
+
+public class MemberController : Controller
+{
+	private readonly SignInManager<AppUser> _signInManager;
+
+
+	public MemberController(SignInManager<AppUser> signInManager)
+	{
+		_signInManager = signInManager;
+	}
+
+	public async Task<IActionResult> LogOut()
+	{
+		await _signInManager.SignOutAsync();
+		return RedirectToAction("Index", "Home");
+	}
+
+}
