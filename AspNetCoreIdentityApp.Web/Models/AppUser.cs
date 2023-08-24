c4459c5dc43970e87873c33e45e965b7a5af18d9
Kamran <311.kamran@gmail.com>
2023-08-24 09:47:41 +0400

    UserEditViewModel sinifinin yaradilmasi

--- AspNetCoreIdentityApp.Web/Models/AppUser.cs
+++ AspNetCoreIdentityApp.Web/Models/AppUser.cs
@@ -6,6 +6,6 @@ public class AppUser : IdentityUser
 {
     public string? City { get; set; }
     public string? Picture { get; set; }
-    public DateTime? BirthDay { get; set; }
+    public DateTime? BirthDate { get; set; }
     public byte? Gender { get; set; }
 }
---
+++ AspNetCoreIdentityApp.Web/ViewModels/UserEditViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels;
+
+public class UserEditViewModel
+{
+	[Required(ErrorMessage = "Kullanıcı Ad alanı boş bıraxılamaz.")]
+	[Display(Name = "Kullanıcı Adi: ")]
+	public string Username { get; set; } = null!;
+
+	[EmailAddress(ErrorMessage = "Email formati yanlistir")]
+	[Required(ErrorMessage = "Kullanıcı Email alanı boş bıraxılamaz.")]
+	[Display(Name = "Email: ")]
+	public string Email { get; set; } = null!;
+
+	[Required(ErrorMessage = "Kullanıcı Telefon alanı boş bıraxılamaz.")]
+	[Display(Name = "Telefon: ")]
+	public string Phone { get; set; } = null!;
+
+	[Display(Name = "Doğum tarixi : ")]
+	public DateTime? BirtDate { get; set; }
+
+	[Display(Name = "Doğum Tarixi : ")]
+	public string? City { get; set; }
+
+	[Display(Name = "Profil resmi : ")]
+	public IFormFile? Picture { get; set; }
+
+	[Display(Name = "Cinsiyyət: ")]
+	public byte? Gender { get; set; }
+}
