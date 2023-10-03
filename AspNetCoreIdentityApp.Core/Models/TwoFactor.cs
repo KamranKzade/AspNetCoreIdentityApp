841d392679eea59f180325cabd4b7a1f721384ff
Kamran <311.kamran@gmail.com>
2023-10-03 11:48:42 +0400

    Two Factor Enum-inin yaradilmasi

---
+++ AspNetCoreIdentityApp.Core/Models/TwoFactor.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Core.Models;
+
+public enum TwoFactor
+{
+	[Display(Name ="Heç biri")]
+	None = 0,
+	[Display(Name ="Telofon ilə kimlik doğrulama")]
+	Phone = 1,
+	[Display(Name ="Email ilə kimlik doğrulama")]
+	Email = 2,
+	[Display(Name ="Microsoft/Google Authenticator ilə kimlik doğrulama")]
+	MicrosoftGoogle = 3
+}
